4076e270695f5ea2d771c45a353e3bcc4a3bb9d9
Matt Marshall <matt.marshallian@gmail.com>
2018-06-23 19:55:24 +0200

    Sprawdzenie inicjalizacji grafiki SDL

--- src/SDL Basic mingw.cpp
+++ src/SDL Basic mingw.cpp
@@ -23,6 +23,15 @@
 using namespace std;
 
 int main() {
+
+	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+		cout << "SDL screwed up" << endl;
+		return 1;
+	}
+
 	cout << "Hello World with SDL2" << endl;
+
+	SDL_Quit();
+
 	return 0;
 }
