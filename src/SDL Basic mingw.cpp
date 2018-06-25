0e2adff7435ba5d9beadc3b1205e089c99513502
Matt Marshall <matt.marshallian@gmail.com>
2018-06-25 20:52:09 +0200

    Added renderer, buffer and texture

--- src/SDL Basic mingw.cpp
+++ src/SDL Basic mingw.cpp
@@ -13,6 +13,7 @@
 */
 
 #include <iostream>
+#include <cstring>
 #include <SDL.h>
 
 // #undef poni¿ej to jakaœ sztuczka z
@@ -35,17 +36,44 @@ int main() {
 	cout << "Hello World with SDL2" << endl;
 
 	SDL_Window *window = SDL_CreateWindow("Particle Fire Explosion",
-			SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
+			SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+			WINDOW_WIDTH, WINDOW_HEIGHT,
+			SDL_WINDOW_SHOWN);
 
 	if (nullptr == window) {
 		SDL_Quit();
 		return 2;
 	}
 
+	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_PRESENTVSYNC);
+	SDL_Texture *texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888,
+			SDL_TEXTUREACCESS_STATIC, WINDOW_WIDTH, WINDOW_HEIGHT);
+
+	if (nullptr == renderer) {
+		cout << "Cannot create renderer " << endl;
+		SDL_DestroyWindow(window);
+		SDL_Quit();
+		return 3;
+	}
+
+	if (nullptr == texture) {
+		cout << "Cannot create texture " << endl;
+		SDL_DestroyRenderer(renderer);
+		SDL_DestroyWindow(window);
+		SDL_Quit();
+		return 4;
+	}
+
+	Uint32 *buffer = new Uint32[WINDOW_WIDTH * WINDOW_HEIGHT];
+	memset(buffer, 192, WINDOW_WIDTH * WINDOW_HEIGHT * sizeof(Uint32));
+
+	SDL_UpdateTexture(texture, NULL, buffer, WINDOW_WIDTH * sizeof(Uint32));
+	SDL_RenderClear(renderer);
+	SDL_RenderCopy(renderer, texture, NULL, NULL);
+	SDL_RenderPresent(renderer);
+
 	SDL_Event event;
-
 	bool quit = false;
-
 	while (!quit) {
 		// Upgrade particles
 		// Draw particles
@@ -58,7 +86,10 @@ int main() {
 		};
 	}
 
-
+	delete[] buffer;
+	SDL_DestroyTexture(texture);
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
 	SDL_Quit();
 
 	return 0;
