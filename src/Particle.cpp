69d672847b62d46fa6b52b1fd9f9894623d44740
Matt Marshall <matt.marshallian@gmail.com>
2018-07-04 06:28:13 +0200

    Efekt wybuchu ze zmniejszającą się prędkością cząstek

--- src/Particle.cpp
+++ src/Particle.cpp
@@ -13,7 +13,9 @@ namespace sdlbasic {
 
 Particle::Particle() :m_x(0), m_y(0) {
 	m_direction = (2.0 * M_PI * rand()) / RAND_MAX;
-	m_speed = (0.0001 * rand()) / RAND_MAX;
+	m_speed = (0.02 * rand()) / RAND_MAX;
+
+	m_speed *= m_speed;
 }
 
 Particle::~Particle() {
